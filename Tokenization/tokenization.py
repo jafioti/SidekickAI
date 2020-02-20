61154794cf2be066588162d48b5abf72662ddf4c
jafioti <jafioti@gmail.com>
2020-02-19 22:07:19 -0500

    Changed tokenizer

--- Tokenization/tokenization.py
+++ Tokenization/tokenization.py
@@ -1,27 +1,23 @@
-import tokenizers
-
 spacyNLP = None
 tokenizer = None
 
 # SPACY SUPPORT FUNCTIONS
 def check_spacy():
-    import spacy
-    if spacyNLP == None:
-        spacyNLP = spacy.load("en_core_web_sm") # defaults to spacy small model if none is previously selected
+    load_spacy_small() # defaults to spacy small model if none is previously selected
 
-def load_spacy_small():
-    import spacy
-    if spacyNLP == None:
+def load_spacy_small(overwrite=False):
+    if spacyNLP == None or overwrite:
+        import spacy
         spacyNLP = spacy.load("en_core_web_sm")
 
-def load_spacy_med():
-    import spacy
-    if spacyNLP == None:
+def load_spacy_med(overwrite=False):
+    if spacyNLP == None or overwrite:
+        import spacy
         spacyNLP = spacy.load("en_core_web_md")
 
-def load_spacy_large():
-    import spacy
-    if spacyNLP == None:
+def load_spacy_large(overwrite=False):
+    if spacyNLP == None or overwrite:
+        import spacy
         spacyNLP = spacy.load("en_core_web_lg")
 
 # HUGGINGFACE SUPPORT FUNCTIONS
@@ -29,26 +25,31 @@ def check_huggingface():
     if tokenizer = None:
         load_bert_wordpiece()
 
-def load_bert_wordpiece(lowercase=True):
-    from tokenizers import BertWorkPieceTokenizer
-    tokenizer = BertWordPieceTokenizer("bert-base-uncased-vocab.txt", lowercase=lowercase)
+def load_bert_wordpiece(lowercase=True, overwrite=False):
+    if tokenizer == None or overwrite:
+        from tokenizers import BertWorkPieceTokenizer
+        tokenizer = BertWordPieceTokenizer("bert-base-uncased-vocab.txt", lowercase=lowercase)
 
-def load_sentencepiece():
-    from tokenizers import SentencePieceBPETokenizer
-    tokenizer = SentencePieceBPETokenizer()
+def load_sentencepiece(lowercase=True, overwrite=False):
+    if tokenizer == None or overwrite:
+        from tokenizers import SentencePieceBPETokenizer
+        tokenizer = SentencePieceBPETokenizer()
 
 
 # TOKENIZING FUNCTIONS
-def spacy_tokenize(sentence, text=False):
-    check_spacy()
-    if text:
-        return [token.text for token in spacyNLP(sentence)]
-    else:
-        return spacyNLP(sentence)
+def tokenize(sentence, text=False):
+    # Check if there is no initialized tokenizer
+    if tokenizer == None and spacyNLP == None:
+        check_huggingface() # Favor huggingface since it is faster and more flexible
 
-def tokenizeHuggingface(sentence, text=False):
-    check_huggingface()
-    if text:
-        return(tokenizer.encode(sentence).tokens)
-    else:
-        return(tokenizer.encode(sentence))
+    # Do tokenization based on avaliable tokenizer
+    if tokenizer != None:
+        if text:
+            return(tokenizer.encode(sentence).tokens)
+        else:
+            return(tokenizer.encode(sentence))
+    else if spacyNLP != None:
+        if text:
+            return [token.text for token in spacyNLP(sentence)]
+        else:
+            return spacyNLP(sentence)
