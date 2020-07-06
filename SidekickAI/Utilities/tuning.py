99bb989fa27c57b1bd935d16827d4539cef6b608
Joe Fioti <jafioti@gmail.com>
2020-07-05 22:46:59 -0400

    Added grid tuner and docstrings to other tuners

--- SidekickAI/Utilities/tuning.py
+++ SidekickAI/Utilities/tuning.py
@@ -1,4 +1,4 @@
-import random, math, inspect, sys, os
+import random, math, inspect, sys, os, copy
 from torch.utils.tensorboard import SummaryWriter
 import torch
 
@@ -10,6 +10,16 @@ def filter_args_dict_to_function(args_dict, function): # Helper function to filt
 # Hyperparameter tuner that uses a genetic algorithim to evolve the hyperparameters
 # Hyperparameters to be adjusted are to be passed in as a range (floats are chosen as floats, while ints are chosen as ints)
 def evolution_tune(model_class, train_function, hyperparameters, other_parameters, population_size, generations, explore_outside_bounds=True, print_training_progress=False):
+    '''A hyperparameter tuner that uses a genetic algorithim\n
+    Inputs:
+        model_class (class): The class of the model (not an instance)
+        train_function (function): The training function, which handles initializing the optimizer and any lr scheduling as well
+        hyperparameters (dict): All of the hyperparameters to be tuned, passed into either the model or the train function. Ones to be adjusted are to be passed in as a range (floats are chosen as floats, while ints are chosen as ints)
+        other_parameters (dict): Any other parameters to be passed into either the model or the train function
+        population_size (int): The size of the population of hyperparameter samples for each generation
+        generations (int): The number of generations to search for
+        explore_outside_bounds (bool) [default=True]: Whether to search the parameters outside given bounds in subsequent generations
+        print_training_progress (bool) [default=False]: Whether to allow the training function to print'''
     print("Tuning Hyperparameters Through Evolution...")
     # Create initial population
     population = []
@@ -57,21 +67,31 @@ def evolution_tune(model_class, train_function, hyperparameters, other_parameter
                     population[-1][keys[hyperparameter]] = top_genome[keys[hyperparameter]]
 
 # Random search over hyperparameters
-def random_tune(model_class, train_function, hyperparameters, other_parameters, num_samples, print_training_progress=False, top_samples_to_print=None):
+def random_tune(model_class, train_function, hyperparameters, other_parameters, num_samples, print_training_progress=False, top_samples_to_print=3):
+    '''A hyperparameter tuner that chooses random samples of hyperparameters\n
+    Inputs:
+        model_class (class): The class of the model (not an instance)
+        train_function (function): The training function, which handles initializing the optimizer and any lr scheduling as well
+        hyperparameters (dict): All of the hyperparameters to be tuned, passed into either the model or the train function. Ones to be adjusted are to be passed in as a range (floats are chosen as floats, while ints are chosen as ints)
+        other_parameters (dict): Any other parameters to be passed into either the model or the train function
+        num_samples (int): The total number of samples to test
+        print_training_progress (bool) [default=False]: Whether to allow the training function to print
+        top_samples_to_print (int) [default=3]: The number of top samples to print at the end of testing'''
     print("Running Random Search Over Hyperparameters...")
+    top_samples_to_print = min(top_samples_to_print, num_samples) # Limit the print samples to the number of samples we are testing
     # Pick samples
     samples = []
     keys = list(hyperparameters.keys())
     for sample in range(num_samples):
-        sample.append({})
+        samples.append({})
         for hyperparameter in range(len(keys)):
             if isinstance(hyperparameters[keys[hyperparameter]], list):
                 if isinstance(hyperparameters[keys[hyperparameter]][0], int) and isinstance(hyperparameters[keys[hyperparameter]][0], int):
-                    sample[keys[hyperparameter]] = random.randint(hyperparameters[keys[hyperparameter]][0], hyperparameters[keys[hyperparameter]][1])
+                    samples[-1][keys[hyperparameter]] = random.randint(hyperparameters[keys[hyperparameter]][0], hyperparameters[keys[hyperparameter]][1])
                 else:
-                    sample[keys[hyperparameter]] = random.uniform(hyperparameters[keys[hyperparameter]][0], hyperparameters[keys[hyperparameter]][1])
+                    samples[-1][keys[hyperparameter]] = random.uniform(hyperparameters[keys[hyperparameter]][0], hyperparameters[keys[hyperparameter]][1])
             else:
-                sample[keys[hyperparameter]] = hyperparameters[keys[hyperparameter]]
+                samples[-1][keys[hyperparameter]] = hyperparameters[keys[hyperparameter]]
     
     # Run samples through training
     fitnesses = []
@@ -102,12 +122,29 @@ def random_tune(model_class, train_function, hyperparameters, other_parameters, 
 
 # Grid search over hyperparameters
 def grid_tune(model_class, train_function, hyperparameters, other_parameters, print_training_progress=False, top_samples_to_print=None):
+    '''A hyperparameter tuner that runs grid search on all possible hyperparameter combinations\n
+    Inputs:
+        model_class (class): The class of the model (not an instance)
+        train_function (function): The training function, which handles initializing the optimizer and any lr scheduling as well
+        hyperparameters (dict): All of the hyperparameters to be tuned, passed into either the model or the train function. Ones to be adjusted are to be passed in as a list of discrete values
+        other_parameters (dict): Any other parameters to be passed into either the model or the train function
+        print_training_progress (bool) [default=False]: Whether to allow the training function to print
+        top_samples_to_print (int) [default=3]: The number of top samples to print at the end of testing'''
     print("Running Grid Search Over Hyperparameters...")
     # Pick samples
-    samples = []
+    samples = [copy.deepcopy(hyperparameters)]
     keys = list(hyperparameters.keys())
-    
-    
+    for hyperparameter in range(len(keys)):
+        # If this is a list of steps, make a new configuration if we have not already added it
+        if isinstance(hyperparameters[keys[hyperparameter]], list):
+            for config in range(len(hyperparameters[keys[hyperparameter]])):
+                current_config = copy.deepcopy(hyperparameters)
+                current_config[keys[hyperparameter]] = hyperparameters[keys[hyperparameter]][config]
+                # Check if we already added this config
+                if current_config not in samples: samples.append(current_config)
+                
+    top_samples_to_print = min(top_samples_to_print, num_samples) # Limit the print samples to the number of samples we are testing
+
     # Run samples through training
     fitnesses = []
     for sample in range(len(samples)):
